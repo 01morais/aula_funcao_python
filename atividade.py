a790cc94c92b8214d9d7b5f5c8747bc47b207003
Matheus Morais <matheusmorais_s@hotmail.com>
2026-03-27 21:45:13 -0300

    versão 1
    Aula + atividade

---
+++ atividade.py
@@ -0,0 +1,6 @@
+def calcular_media(nota1, nota2, nota3):
+    media = (nota1 + nota2 + nota3):
+    return media
+
+print("sua media é:", calcular_media(5, 8, 9))
+
---
+++ aula.py
@@ -0,0 +1,13 @@
+print("funções")
+
+# criação da funçaõ
+def saudacao(nome):
+    print(f"Bem vindo a TI {nome}")
+
+def somar_numeros(num1, num2):
+    
+saudacao("Maria")
+
+print("Resultado:", somar_numeros(8,9))
+
+
